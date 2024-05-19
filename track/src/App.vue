49aa52308da2b84195d6691d710f0e2feb4050d8
Yaroslav <yaroslavv86@gmail.com>
2024-05-19 23:35:55 +0500

    first commit

---
+++ track/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+import TheHeader from './components/TheHeader.vue'
+import TheNav from './components/TheNav.vue'
+import TheTimeLine from './pages/TheTimeLine.vue'
+import TheActivities from './pages/TheActivities.vue'
+import TheProgress from './pages/TheProgress.vue'
+import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from './constants'
+
+let currentPage = ref(normilizePageHash())
+
+function normilizePageHash() {
+  const hash = window.location.hash.slice(1)
+
+  if ([PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE].includes(hash)) return hash
+
+  window.location.hash = PAGE_TIMELINE
+
+  return PAGE_TIMELINE
+}
+</script>
+
+<template>
+  <TheHeader />
+
+  <main class="flex flex-grow flex-col">
+    <TheTimeLine v-show="currentPage === PAGE_TIMELINE" />
+    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
+    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
+  </main>
+
+  <TheNav :current-page="currentPage" @navigate="currentPage = $event" />
+</template>
+
+<style scoped></style>
---
+++ track/src/components/TheHeaderProgress.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+  <a class="text-sm" href="#">
+    <div v-if="false" class="flex items-center gap-1">
+      Day cpmplete
+
+      <CheckCircleIcon class="h-7 text-green-500" />
+    </div>
+
+    <div v-else class="flex items-center gap-1">
+      <div>Progress: <span class="font-mono">20%</span></div>
+      <div class="h-3 w-3 rounded-full bg-red-500"></div>
+    </div>
+  </a>
+</template>
